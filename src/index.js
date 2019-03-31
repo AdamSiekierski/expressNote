dab1a2a32f167a326170aa0fed617f76f6112914
AdamSiekierski <adam.siekiera@outlook.com>
2019-03-31 21:33:26 +0200

    Fixed CORS

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 3333;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
 app.disable('x-powered-by');
 
 app.use('/', router);
