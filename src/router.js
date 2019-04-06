01beb0d6ffefc0f61ce3bd1e34df71473a83a11a
AdamSiekierski <adam.siekiera@outlook.com>
2019-04-06 09:09:54 +0200

    Added delete route

--- src/router.js
+++ src/router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 import notesRoute from './routes/notes'
 import searchRoute from './routes/search'
 import addRoute from './routes/add'
+import deleteRoute from './routes/delete'
 
 router.use('/notes', notesRoute);
 router.use('/search', searchRoute);
 router.use('/add', addRoute);
+router.use('/delete', deleteRoute);
 
 export default router;
---
+++ src/routes/delete.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import {body, validationResult} from 'express-validator'
+import Database from '../database'
+const router = express.Router();
+
+router.post('/', body('id').exists(), (req, res) => {
+    if (!validationResult.isEmpty()) {
+        return res.status(422).json({
+            message: "Did not pass the ID of note to delete"
+        })
+    }
+    Database.sendQuery(`DELETE FROM notes WHERE id=${req.body.id}`).then((result) => {
+        res.json({
+            status: 'success',
+            message: `Deleted note with id: ${req.body.id}`,
+            databaseResponse: result
+        })
+    })
+})
+
+export default router
