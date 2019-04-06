e5a75b9150b8101fc53455d02cbaeb04d13c640e
AdamSiekierski <adam.siekiera@outlook.com>
2019-04-06 17:01:28 +0200

    Small change

--- src/index.js
+++ src/index.js
@@ -9,12 +9,6 @@ const port = process.env.PORT || 3333;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
-
 app.disable('x-powered-by');
 
 app.use('/', router);
